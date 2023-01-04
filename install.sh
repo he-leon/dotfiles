46ca1740dd8ace978e6a0a6f753728006878918f
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 17:43:55 +0100

    Added Xmodmap

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -sf $PWD/zsh/zshrc $HOME/.zshrc
 ln -sf $PWD/zsh/zshrc_base $HOME/.zshrc_base
 # NVIM
-ln -s $PWD/nvim $HOME/.config/nvim
-# Xresources
-ln -s $PWD/X/Xresources $HOME/.Xresources
+ln -sf $PWD/nvim $HOME/.config/nvim
+# X
+ln -sf $PWD/X/Xresources $HOME/.Xresources
+ln -sf $PWD/X/Xmodmap $HOME/.Xmodmap
