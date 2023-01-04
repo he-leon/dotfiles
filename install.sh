6c79186bc44620d72051beccc10fc20d3190e2bb
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 21:48:15 +0100

    Added xfce

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
-# ZSH
+# This script links the dotfiles to the according files in this git repo
+# zsh
 ln -sf $PWD/zsh/zshrc $HOME/.zshrc
 ln -sf $PWD/zsh/zshrc_base $HOME/.zshrc_base
-# NVIM
-ln -sf $PWD/nvim $HOME/.config/nvim
+# nvim
+ln -sf $PWD/nvim -t $HOME/.config/
 # X
 ln -sf $PWD/X/Xresources $HOME/.Xresources
 ln -sf $PWD/X/Xmodmap $HOME/.Xmodmap
+# xfce
+ln -sf $PWD/xfce/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+# byobu
+ln -sf $PWD/byobu/*.tmux -t $HOME/.byobu/
