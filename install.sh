6ddb1f21afafb98f5fb40b6a398442f01a9f477b
l.heizinger <l.heizinger@consolinno.de>
2023-08-31 10:00:39 +0200

    Added nvim_conf shortcut

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ ln -sf $PWD/X/Xmodmap $HOME/.Xmodmap
 ln -sf $PWD/xfce/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 # byobu
 ln -sf $PWD/byobu/*.tmux -t $HOME/.byobu/
+# Config shortcut
+ln -sf $PWD/nvim_conf.sh $HOME/.local/bin/nvim_conf
