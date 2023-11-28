2e834fafb4debd3271d591c2b38db9ad930ed3cb
l.heizinger <l.heizinger@consolinno.de>
2023-11-28 10:20:54 +0100

    Added script to install nvim as appimage

---
+++ install_nvim_appimage.sh
@@ -0,0 +1,5 @@
+wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O ~/.local/bin/nvim.appimage
+chmod u+x ~/.local/bin/nvim.appimage
+sudo ln -sf ~/.local/bin/nvim.appimage /usr/bin/nvim  
+
+
